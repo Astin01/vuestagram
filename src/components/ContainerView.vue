eb0a70a4c07d3a03aa98e0815242c67a260bc9ed
astin01 <wonhun1225@gmail.com>
2023-03-22 13:21:12 +0900

    vuestagram

---
+++ src/components/ContainerView.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>
+  <PostView/>
+  <PostView/>
+  <PostView/>
+</div>
+</template>
+
+<script>
+import PostView from './PostView.vue';
+export default {
+    name:"ContainerView",
+    components:{
+      PostView
+    }
+}
+</script>
+
+<style>
+
+</style>
